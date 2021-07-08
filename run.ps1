8cbe6b117fd42fcd2482c6b8cba7a25cb9ef817e
Sergey Zheltov <zheltov.sergey1@gmail.com>
2021-07-08 15:39:56 +0300

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Continue'
+
+& ${env:CONDA}\condabin\conda.bat activate base
+
+$cmd_args = @"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 &&
+cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=../cmake/windows-dpcpp-toolchain.cmake -DCMAKE_CXX_COMPILER=dpcpp -DONEDPL_BACKEND=dpcpp -DONEDPL_DEVICE_TYPE=CPU .. &&
+ninja -j -v &&
+ctest --timeout 360 --output-on-failure
+"@
+
+${cmd_args} = [string]::join(" ", (${cmd_args}.Split("`n")))
+cmd /c ${cmd_args}
+exit ${LastExitCode}
